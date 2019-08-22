ea541f27d0500902902846d1a11f04abd289f863
mmy <mmy_xize@163.com>
2019-08-22 22:56:54 +0800

    提交practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,42 @@
 package practice06;
 
-public class Teacher {
+import java.lang.reflect.Field;
+
+public class Teacher extends Person{
+	private int klass;
+
+	public Teacher(String name, int age) {
+		super(name, age);
+	}
+	
+ 	public Teacher(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+	
+	public String introduce() {
+		String fieldname = "klass";
+		Field[] fields = this.getClass().getDeclaredFields();
+		boolean isKlassExist = false;
+		for (int i = 0; i < fields.length; i++) {
+			if (fields[i].getName().equals(fieldname)) {
+				isKlassExist = true;
+				break;
+			}
+		}
+		
+		if (isKlassExist) {
+			return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+		} else {
+			return super.introduce() + " I am a Teacher. I teach No Class.";
+		}
+	}
 }
