3e2ae6b67d2bb211e06252561a972d32d35b4fed
mmy <mmy_xize@163.com>
2019-08-23 10:33:17 +0800

    提交practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+	private int number;
+	private Person leader;
+	
+	public Klass(int number) {
+		this.number = number;
+	}
+	
+	public void assignLeader(Student student) {
+		this.leader = student;
+	}
+	
+	public Person getLeader() {
+		return this.leader;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+	
+	public String getDisplayName() {
+		return "Class " + this.number;
+	}
 }
